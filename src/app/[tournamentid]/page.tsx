fb0892288ba82a9fda58885d0fe2c5b6ae83dca3
Tadeas Kozub <tedkozub@github.com>
2024-10-02 14:57:39 +0200

    wip: match table

--- src/app/[tournamentid]/page.tsx
+++ src/app/[tournamentid]/page.tsx
@@ -6,7 +6,27 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        Unikatny prehled matchu v turnaji {params.tournamentid}
+        Unikatny prehled matchu v turnaji {params.tournamentid}|{" "}
+        <table>
+          <tr>
+            <th>Sloupec 1</th>
+            <th>Sloupec 2</th>
+            <th>Sloupec 3</th>
+          </tr>
+          <tr>
+            <td>Řádek 1, Buňka 1</td>
+            <td>Řádek 1, Buňka 2</td>
+            <td>Řádek 1, Buňka 3</td>
+          </tr>
+          <tr>
+            <td colSpan={2}>Řádek 2, Buňka 1 (spanuje 2 sloupce)</td>
+            <td>Řádek 2, Buňka 2</td>
+          </tr>
+          <tr>
+            <td>Řádek 3, Buňka 1</td>
+            <td colSpan={2}>Řádek 3, Buňka 2 (spanuje 2 sloupce)</td>
+          </tr>
+        </table>
       </main>
     </div>
   );
