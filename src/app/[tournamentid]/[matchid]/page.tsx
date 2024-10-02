87e2a8d45e0596b08a6ed4cc5f4b2f18ca1db443
Petr Hybl <phybl@redhat.com>
2024-10-02 13:09:39 +0200

    added basic routing

--- src/app/[tournamentid]/[matchid]/page.tsx
+++ src/app/[tournamentid]/[matchid]/page.tsx
@@ -1,8 +1,12 @@
+"use client";
+import { useParams } from "next/navigation";
+
 export default function Home() {
+  const params = useParams();
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        Prehled turnaju
+        Unikatny prehled matchu v turnaji {params.id}
       </main>
     </div>
   );
