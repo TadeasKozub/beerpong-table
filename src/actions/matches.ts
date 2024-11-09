29df41768ffab48ace2a3d370bbdb2d3659cae3e
Petr Hybl <phybl@redhat.com>
2024-11-09 13:42:30 +0100

    added delete button for matches

--- src/actions/matches.ts
+++ src/actions/matches.ts
@@ -3,6 +3,7 @@
 import { db } from "@/db";
 import { match } from "@/db/schema";
 import { eq } from "drizzle-orm";
+import { redirect } from "next/navigation";
 
 export const createNewMatch = async (
   tournamentId: number,
@@ -22,4 +23,9 @@ export const getMatchesForTournament = async (tournamentId: number) => {
     .from(match)
     .where(eq(match.tournament_id, tournamentId));
   return matches;
+};
+
+export const deleteMatch = async (id: number) => {
+  const tournament_id = await db.delete(match).where(eq(match.id, id)).returning({ deletedId: match.tournament_id });
+  redirect("/" + tournament_id[0].deletedId); 
 };
--- src/components/matchesOverviewTable.tsx
+++ src/components/matchesOverviewTable.tsx
@@ -1,8 +1,10 @@
+"use client";
+import { deleteMatch } from "@/actions/matches";
 import React from "react";
 
 interface MatchWithTeams {
   match: {
-    id: string;
+    id: number;
     score: string;
   };
   team1: {
@@ -34,6 +36,7 @@ export const MatchesOverViewTable: React.FC<MatchesOverViewTableProps> = ({
             <th className="px-6 py-3 text-left text-xs font-medium text-white uppercase tracking-wider">
               Team Name
             </th>
+            <th></th>
           </tr>
         </thead>
         <tbody className="bg-black divide-y divide-gray-200">
@@ -45,6 +48,25 @@ export const MatchesOverViewTable: React.FC<MatchesOverViewTableProps> = ({
               </td>
               <td className="px-6 py-4 whitespace-nowrap">
                 {data.team2.name || "N/A"}
+              </td>
+              <td className="text-center">
+                <button
+                  onClick={() => deleteMatch(data.match.id)}
+                  className="text-red-600 hover:text-red-900 flex items-center justify-center w-full"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-6 w-6"
+                    viewBox="0 0 24 24"
+                    fill="red"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M18.364 5.636a1 1 0 010 1.414L13.414 12l4.95 4.95a1 1 0 11-1.414 1.414L12 13.414l-4.95 4.95a1 1 0 01-1.414-1.414L10.586 12 5.636 7.05a1 1 0 111.414-1.414L12 10.586l4.95-4.95a1 1 0 011.414 0z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                </button>
               </td>
             </tr>
           ))}
