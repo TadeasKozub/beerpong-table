e64128c0c1e63ab7ba5a88ff96097a8335ecd1c8
Tadeas Kozub <tedkozub@github.com>
2024-11-09 02:15:24 +0100

    add basic auth with middleware

---
+++ src/actions/auth.ts
@@ -0,0 +1,32 @@
+"use server";
+
+import { redirect } from "next/navigation";
+import { cookies } from "next/headers";
+import { createSession, decrypt } from "@/lib/session";
+
+export async function login(formData: FormData) {
+  const adminUsername = process.env.ADMIN_USERNAME;
+  const adminPassword = process.env.ADMIN_PASSWORD;
+  if (!(formData.get("name") === adminUsername)) {
+    return;
+  }
+  if (!(formData.get("password") === adminPassword)) {
+    return;
+  }
+  // create session
+  await createSession("1");
+
+  console.log("login succesful");
+}
+
+export async function logout() {
+  // deleteSession();
+  cookies().delete("session");
+  redirect("/login");
+}
+
+export async function getSession() {
+  const cookie = cookies().get("session")?.value;
+  const session = await decrypt(cookie);
+  return session;
+}
